c1ef4d8da604a3c015db18323a804b44c5016ed2
sanyangDev <songhyeonseo@gmail.com>
2023-04-17 18:08:10 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 	const [counter, setCounter] = useState(0);
-	const onClick = () => setCounter(event => event + 1);
+	const [search, setSearch] = useState("");
+	const onClick = () => setCounter(prev => prev + 1);
+	const onChange = event => setSearch(event.target.value);
+	console.log("always");
+	useEffect(() => {
+		console.log("call the api");
+	}, []);
 	return (
 		<div>
+			<input value={search} onChange={onChange} type="text" placeholder="Search hear..." />
 			<h1>{counter}</h1>
 			<button onClick={onClick}>Click!</button>
 		</div>
