f1ff95cc9bc483fe65779b73fbcbd82a5d0f50b2
sanyangDev <songhyeonseo@gmail.com>
2023-04-24 21:32:58 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import { useEffect, useState } from "react";
 
 function App() {
-	const [counter, setCounter] = useState(0);
-	const [search, setSearch] = useState("");
-	const onClick = () => setCounter(prev => prev + 1);
-	const onChange = event => setSearch(event.target.value);
-	useEffect(() => {
-		console.log("only once");
-	}, []);
-	useEffect(() => {
-		console.log("counter call");
-	}, [counter]);
-	useEffect(() => {
-		if (search !== "") {
-			console.log("search call");
+	const [toDo, setToDo] = useState("");
+	const [toDos, setToDos] = useState([]);
+	const onChange = event => setToDo(event.target.value);
+	const onSubmit = event => {
+		event.preventDefault();
+		if (toDo === "") {
+			return;
 		}
-	}, [search]);
+		console.log(toDos);
+		setToDos(currentArray => [toDo, ...currentArray]);
+		setToDo("");
+	};
 	return (
 		<div>
-			<input value={search} onChange={onChange} type="text" placeholder="Search hear..." />
-			<h1>{counter}</h1>
-			<button onClick={onClick}>Click!</button>
+			<form onSubmit={onSubmit}>
+				<h4>{toDos}</h4>
+				<input onChange={onChange} value={toDo} type="text" placeholder="What is your todo?" />
+				<button>Go</button>
+			</form>
 		</div>
 	);
 }
